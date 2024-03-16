b1a516b582fe40543815ab1eb545a4b4c99794c9
Piotr Preciuk <piotr.preciukbusiness@gmail.com>
2024-03-16 19:05:39 +0100

    changed to ts

--- app/deploy.ts
+++ app/deploy.ts
@@ -1,13 +1,25 @@
-const ethers = require("ethers");
-const fs = require("fs-extra");
-require("dotenv").config();
+import { ethers } from "ethers";
+import * as fs from "fs-extra";
+import "dotenv/config";
+
+interface GreeterContract extends ethers.Contract {
+    //@ts-ignore
+    greet(): Promise<string>;
+    //@ts-ignore
+    getBalance(account: string): Promise<ethers.BigNumber>;
+    //@ts-ignore
+    sendEth(
+        receiver: string,
+        transactionDetails: ethers.TransactionRequest,
+    ): Promise<ethers.ContractTransaction>;
+}
 
 async function TestContract() {
-    const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
+    const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY!;
     const provider = new ethers.JsonRpcProvider(
         `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
     );
-    const wallet = new ethers.Wallet(process.env.PRIVATE_KEY_ETHER, provider);
+    const wallet = new ethers.Wallet(process.env.PRIVATE_KEY_ETHER!, provider);
 
     const abi = fs.readFileSync(
         "compilation/contracts_Test_sol_Greeter.json",
@@ -20,9 +32,9 @@ async function TestContract() {
 
     const contractFactory = new ethers.ContractFactory(abi, bytecode, wallet);
 
-    const contract = await contractFactory.deploy(
+    const contract = (await contractFactory.deploy(
         "This is message from contract :)",
-    );
+    )) as GreeterContract;
     await contract.waitForDeployment();
     console.log("Contract deployed to address:", contract.target);
 
@@ -31,22 +43,20 @@ async function TestContract() {
     console.log(greeting);
 
     const balance = await contract.getBalance(
-        process.env.ACCOUNT_ADDRESS_ETHER,
+        process.env.ACCOUNT_ADDRESS_ETHER!,
     );
     console.log(balance);
 
-    const tx = await contract.sendEth(process.env.RECEIVER_ADDRESS_ETHER, {
-        value: ethers.parseUnits("0.1", "ether"),
+    const tx = await contract.sendEth(process.env.RECEIVER_ADDRESS_ETHER!, {
+        value: ethers.parseUnits("0.01", "ether"),
     });
 
     console.log(tx);
-
-    console.log(balance);
 }
 
 async function main() {
-    // await TestContract();
-    console.log(ethers.parseUnits("0.01", "ether"));
+    await TestContract();
+    // console.log(ethers.parseUnits("0.01", "ether"));
 }
 
 main()
